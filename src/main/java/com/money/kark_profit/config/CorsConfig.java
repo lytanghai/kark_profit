7a4a0a304cf521df553eb1fef1508487f705e4a5
unknown <lytanghaii@gmail.com>
2026-03-13 20:49:30 +0700

    prepr for prod

--- src/main/java/com/money/kark_profit/config/CorsConfig.java
+++ src/main/java/com/money/kark_profit/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.money.kark_profit.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,14 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${app.cors.allowed-methods}")
+    private String allowedMethods;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")               // allow all endpoints
-                        .allowedOrigins("http://localhost:5173") // allow your Vue dev server
-                        .allowedMethods("GET", "POST" )
+
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins))
+                        .allowedMethods(allowedMethods.split(","))
                         .allowCredentials(true);
             }
         };

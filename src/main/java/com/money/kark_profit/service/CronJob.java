b4ff1b724ee40531c822f8dec531dffe4280766e
unknown <lytanghaii@gmail.com>
2026-03-14 15:49:34 +0700

    add cron

---
+++ src/main/java/com/money/kark_profit/service/CronJob.java
@@ -0,0 +1,25 @@
+package com.money.kark_profit.service;
+
+import com.money.kark_profit.http.RestTemplateHttpClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CronJob {
+
+    private final RestTemplateHttpClient restHttp;
+
+    private final String url = "https://alert-engine-h1mv.onrender.com/health";
+
+    @Scheduled(fixedRate = 100000)
+    public void sendMessage() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("User-Agent", "Mozilla/5.0");
+
+        // Fetch from API
+        String xml = restHttp.get(url, null, headers, String.class);
+    }
+}

2a726875e6ff99e0ed1faab3984cccc67fac3b25
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2026-05-06 11:23:34 +0700

    impl backup script v2

--- src/main/java/com/money/kark_profit/controller/ReportController.java
+++ src/main/java/com/money/kark_profit/controller/ReportController.java
@@ -1,12 +1,14 @@
 package com.money.kark_profit.controller;
 
 import com.money.kark_profit.service.UserService;
+import com.money.kark_profit.service.feature.BackupVersion2Service;
 import com.money.kark_profit.service.feature.ReportService;
 import com.money.kark_profit.transform.request.ReportRequest;
 import com.money.kark_profit.transform.response.ReportResponse;
 import com.money.kark_profit.utils.ResponseBuilderUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +25,19 @@ public class ReportController {
 
     private final ReportService reportService;
     private final UserService userService;
+    private final BackupVersion2Service backupVersion2Service;
 
     @PostMapping("/generate")
     private ResponseEntity<ResponseBuilderUtils<List<ReportResponse>>> generateReport(@RequestBody ReportRequest reportRequest, HttpServletRequest httpServletRequest) {
         return new ResponseEntity<>(reportService.generateReport(
                 reportRequest, userService.extractUserId(httpServletRequest), httpServletRequest), HttpStatus.OK);
+    }
+
+    //for manual backup
+    @SneakyThrows
+    @PostMapping("/script/backup")
+    public void backup() {
+        backupVersion2Service.backup();
     }
 
 }
